bb12679e004bc59a942dd91911b44cbc3e9da5f7
Parazeq <j.kocur14@gmail.com>
2024-10-23 10:59:10 +0200

    2p

--- T00 Zadania/T13 - Funkcje i wybrane biblioteki języków skryptowych/sketch.js
+++ T00 Zadania/T13 - Funkcje i wybrane biblioteki języków skryptowych/sketch.js
@@ -1,9 +1,25 @@
+let x = 100;
 function setup() {
-    createCanvas(600, 600);
+    createCanvas(2000, 700, WEBGL);
 }
 
 function draw() {
-    background(220);
+    background(10);
+    fill('red');
     circle(200, 220, 200);
+    fill('darkblue');
     circle(400, 220, 200);
+    fill('darkviolet');
+    circle(300, 220, 250);
+    translate(-300, 0);
+    rotateY(frameCount * 0.01)
+    fill('darkorange');
+    sphere(80);
+    rotateY(frameCount * 0.05)
+    translate(150, 0);
+    fill('darkgreen');
+    sphere(15)
+    translate(200, 0);
+    fill('darkred');
+    sphere(30)
 }
